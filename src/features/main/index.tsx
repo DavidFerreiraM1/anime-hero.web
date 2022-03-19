ddb12601e77548d5ac5ad6d943277d089b9b7d2c
david-1638634303 <davidferreiram95@gmail.com>
2022-03-18 21:31:48 -0300

    componente input name adicionado

--- src/features/main/index.tsx
+++ src/features/main/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import InputName from './input-name';
 import styles from './main.module.less';
 
 export default function Main() {
   return (
     <div className={styles.root}>
-      <h1>Estamos criando a anime hero</h1>
+      <InputName />
     </div>
   );
 }
