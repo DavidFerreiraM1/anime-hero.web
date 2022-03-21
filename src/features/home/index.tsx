216aabb2e43085148110c835cf1b4bb31a35e713
david-1638634303 <davidferreiram95@gmail.com>
2022-03-21 20:42:01 -0300

    home criada com título e filtro

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import { HomePageProps } from './types';
+import styles from './styles.module.less';
+import Filter from './filter';
+import AnimeListProvider from './context';
 
 export default function Home(props: HomePageProps) {
+  React.useEffect(() => {
+    console.log(props.data.list);
+  }, []);
+
   return (
-    <div>
-      <h1>Olá, {props.username}</h1>
-    </div>
+    <AnimeListProvider>
+      <div className={styles['container-root']}>
+        <div className={styles['container-content']}>
+          <div className={styles['title-root']}>
+            <hgroup>
+              <h1>Bem vindo ao Anime Hero</h1>
+              <h3>Encontre seus animes favoritos aqui</h3>
+            </hgroup>
+          </div>
+          <div>
+            <Filter />
+          </div>
+        </div>
+        {/* <div>
+        {props.data.list.map((item, index) => {
+          return (
+            <div key={index}>
+              <img src={item.attributes.posterImage.small} />
+            </div>
+          );
+        })}
+      </div> */}
+      </div>
+    </AnimeListProvider>
   );
 }
