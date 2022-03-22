56564c80ea056283f95019ae3fb870f040f06038
david-1638634303 <davidferreiram95@gmail.com>
2022-03-22 12:47:55 -0300

    ação de click do item da lista adicionado

--- src/features/home/anime-item/index.tsx
+++ src/features/home/anime-item/index.tsx
@@ -4,14 +4,18 @@ import { AnimeItemProps } from '../types';
 import styles from './styles.module.less';
 
 export default function AnimeItem(props: AnimeItemProps) {
+  const onClick = React.useCallback(() => {
+    props.onClick(props.data.id);
+  }, [props.data]);
+
   return (
-    <div role="link" className={styles['root']}>
+    <div role="link" className={styles['root']} onClick={onClick}>
       <div className={styles['item-post']}>
         <img alt="anime-banner" src={props.data.attributes.posterImage.small} />
       </div>
       <div className={styles['item-title']}>
         <span className={styles['title-span']}>
-          <span>Título:</span>
+          <span>Título</span>
         </span>
         <span className={styles['title-description']}>
           {props.data.attributes.titles.en}
