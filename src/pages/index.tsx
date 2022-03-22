1bcda356cdb5a8c707d4824c3a5c8d53aa1e7578
david-1638634303 <davidferreiram95@gmail.com>
2022-03-22 13:28:17 -0300

    página inicial alterada alterada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
 /* eslint-disable react/react-in-jsx-scope */
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
+import animeService from '../features/home/services';
 
-import MainPage from '../features/main';
+import HomePage from '../features/home';
+import { AnimeDataType, HomePageProps } from '../features/home/types';
 
-const Home: NextPage = () => {
-  return <MainPage />;
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await animeService.getList(24);
+  const list = await response.json();
+  const notFound = response.status !== 200;
+
+  const props: HomePageProps = {
+    username: 'usuário',
+    data: {
+      list: list.data as Array<AnimeDataType>,
+      offset: 24
+    }
+  };
+
+  return {
+    props,
+    notFound
+  };
+};
+
+const Home: NextPage<HomePageProps> = (props: HomePageProps) => {
+  return <HomePage {...props} />;
 };
 
 export default Home;
