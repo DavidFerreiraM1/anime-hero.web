fd9971b047078fd14123d280d7b09ada34a23e6c
david-1638634303 <davidferreiram95@gmail.com>
2022-03-22 00:08:52 -0300

    item da listagem estilizado

--- src/features/home/anime-item/index.tsx
+++ src/features/home/anime-item/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { AnimeItemProps } from '../types';
 
+import styles from './styles.module.less';
+
 export default function AnimeItem(props: AnimeItemProps) {
   return (
-    <div role="li">
-      <div>
-        <img alt="anime-banner" src={props.data.attributes.slug} />
+    <div role="link" className={styles['root']}>
+      <div className={styles['item-post']}>
+        <img alt="anime-banner" src={props.data.attributes.posterImage.small} />
+      </div>
+      <div className={styles['item-title']}>
+        <span className={styles['title-span']}>
+          <span>Título:</span>
+        </span>
+        <span className={styles['title-description']}>
+          {props.data.attributes.titles.en}
+        </span>
       </div>
     </div>
   );
